6d3ab78735a2f909ac46740ed2618f459554d577
Vijay <jangravijay27@gmail.com>
2020-06-20 01:44:13 +0530

    file uploaded

---
+++ README.md
@@ -0,0 +1,2 @@
+# Handling-Missing-Values-In-Dataset
+Python code to handle missing values in dataset for machine learning
---
+++ missing_values.py
@@ -0,0 +1,16 @@
+from sklearn.preprocessing import Imputer
+import pandas as pd
+
+data=pd.read_csv(r"C:\Users\Lenovo\Downloads\housing\housing.csv")
+#get description of data
+print(data.info())
+#call imputer object
+imputer=Imputer(strategy="median")
+#drop text attributes
+data=data.drop("ocean_proximity",axis=1)
+#fit imputer object to data
+imputer.fit(data)
+#apply imputer to dataset
+data=imputer.transform(data)
+data_pd=pd.DataFrame(data)
+print(data_pd.info())
